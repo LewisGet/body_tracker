2434848d99177135c770cf99b04ecd9ec83ea549
Lewis Jang <lewis.aedes@gmail.com>
2024-09-05 21:11:31 +0800

    add admin panel for record

--- record/admin.py
+++ record/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Finger, Segment
 
-# Register your models here.
+
+@admin.register(Finger)
+class FingerAdmin(admin.ModelAdmin):
+    list_display = ('hand', 'finger_index')
+    list_filter = ('hand', 'finger_index')
+    search_fields = ('hand', 'finger_index')
+
+
+@admin.register(Segment)
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ('finger', 'segment_type', 'x', 'y', 'z', 'timestamp')
+    list_filter = ('finger', 'segment_type')
+    search_fields = ('finger__hand', 'finger__finger_index', 'segment_type')
