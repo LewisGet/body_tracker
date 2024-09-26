1be0ce8aaf8c6e2bfba86c41c1f8a64158fda2e5
Lewis Jang <lewis.aedes@gmail.com>
2024-09-27 02:38:34 +0800

    update smooth frame, merge by finger id and body id part

--- keyframe/views.py
+++ keyframe/views.py
@@ -7,7 +7,7 @@ from django.db.models import Avg
 from django.utils import timezone
 from datetime import timedelta
 
-from record.models import ActionLog
+from record.models import *
 from .models import Keyframe
 from .forms import *
 import json
@@ -60,36 +60,37 @@ class SmoothFrameView(View):
         split_parts = request.GET.get('split_parts', '0.25,0.5,0.75')
         split_parts = [float(point) for point in split_parts.split(',')]
 
-        finger_keyframe_logs = []
-        body_keyframe_logs = []
-        finger_segment_logs = []
-        body_segment_logs = []
+        finger_keyframe_logs = dict()
+        body_keyframe_logs = dict()
+        finger_segment_logs = dict()
+        body_segment_logs = dict()
 
         all_keyframes = Keyframe.objects.all().order_by('timestamp')
         all_finger_logs = ActionLog.objects.filter(finger__isnull=False)
         all_body_logs = ActionLog.objects.filter(head_arm_leg_body__isnull=False)
 
-        for keyframe in all_keyframes:
-            finger_keyframe_logs.extend(self.avg_logs(keyframe.timestamp, time_range, all_finger_logs))
-            body_keyframe_logs.extend(self.avg_logs(keyframe.timestamp, time_range, all_body_logs))
+        all_finger = Finger.objects.all().order_by('id')
+        all_body = HeadArmLegBody.objects.all().order_by('id')
 
+        for finger in all_finger:
+            this_index = str(finger)
+            finger_keyframe_logs[this_index] = []
+            finger_segment_logs[this_index] = []
+            this_finger_logs = all_finger_logs.filter(finger=finger)
 
-        if len(finger_keyframe_logs) == 0:
-            return JsonResponse({'error': "finger action must be present near keyframe timestamp."}, status=404)
+            _keyframe_logs, _segment_logs = self.keyframe_and_segment(all_keyframes, this_finger_logs)
+            finger_keyframe_logs[this_index].extend(_keyframe_logs)
+            finger_segment_logs[this_index].extend(_segment_logs)
 
-        if len(body_keyframe_logs) == 0:
-            return JsonResponse({'error': "body action must be present near keyframe timestamp."}, status=404)
+        for body_part in all_body:
+            this_index = str(body_part)
+            body_keyframe_logs[this_index] = []
+            body_segment_logs[this_index] = []
+            this_body_logs = all_body_logs.filter(head_arm_leg_body=body_part)
 
-        for i in range(len(all_keyframes) - 1):
-            start_time = all_keyframes[i].timestamp
-            end_time = all_keyframes[i + 1].timestamp
-            total_gap = (end_time - start_time).total_seconds() * 1000
-
-            for split in split_parts:
-                base_split_time = start_time + timedelta(milliseconds=total_gap * split)
-
-                finger_segment_logs.extend(self.avg_logs(base_split_time, total_gap * split_range, all_finger_logs))
-                body_segment_logs.extend(self.avg_logs(base_split_time, total_gap * split_range, all_body_logs))
+            _keyframe_logs, _segment_logs = self.keyframe_and_segment(all_keyframes, this_body_logs)
+            body_keyframe_logs[this_index].extend(_keyframe_logs)
+            body_segment_logs[this_index].extend(_segment_logs)
 
         context = {
             'finger_keyframe_logs': finger_keyframe_logs,
@@ -99,6 +100,25 @@ class SmoothFrameView(View):
         }
 
         return JsonResponse(context, status=201)
+
+    def keyframe_and_segment(self, keyframes, logs):
+        keyframe_logs = []
+        segment_logs = []
+
+        for keyframe in keyframes:
+            keyframe_logs.extend(self.avg_logs(keyframe.timestamp, time_range, logs))
+
+        for i in range(len(keyframes) - 1):
+            start_time = keyframes[i].timestamp
+            end_time = keyframes[i + 1].timestamp
+            total_gap = (end_time - start_time).total_seconds() * 1000
+
+            for split in split_parts:
+                base_split_time = start_time + timedelta(milliseconds=total_gap * split)
+                segment_logs.extend(self.avg_logs(base_split_time, total_gap * split_range, logs))
+
+        return keyframe_logs, segment_logs
+
 
     def avg_logs(self, base_timestamp, time_window, logs):
         filtered_logs = []
