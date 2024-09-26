066eb6df6bb3f2ad115779f2b6a1101359844662
Lewis Jang <lewis.aedes@gmail.com>
2024-09-26 21:53:31 +0800

    add camera script

---
+++ hardware/image_log_camera.py
@@ -0,0 +1,53 @@
+import cv2
+import time
+import datetime
+import requests
+from bs4 import BeautifulSoup
+
+
+# 設置 API 網址
+api_url = "http://127.0.0.1:8000/record/image_log/create"
+
+# 獲取 CSRF token
+def get_csrf_token():
+    response = requests.get(api_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    csrf_token = soup.find('input', {'name': 'csrfmiddlewaretoken'})['value']
+
+    return csrf_token
+
+
+def create_image_post(frame, timestamp):
+    _, img_encoded = cv2.imencode('.jpg', frame)
+    csrf = get_csrf_token()
+
+    files = {'image': ('image.jpg', img_encoded.tobytes(), 'image/jpeg')}
+    data = {'timestamp': timestamp, 'csrfmiddlewaretoken': csrf}
+    headers = {'X-CSRFToken': csrf}
+
+    response = requests.post(api_url, files=files, data=data)
+
+    return response
+
+
+# init cam
+cap = cv2.VideoCapture(0)
+
+# test available cam
+if not cap.isOpened():
+    print("cam init fail")
+    exit()
+
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("cam no signal")
+        break
+
+    timestamp = datetime.datetime.fromtimestamp(float(time.time()))
+    cv2.imshow('USB Camera', frame)
+
+    create_image_post(frame, timestamp)
+    time.sleep(1000 / 40 / 1000)
